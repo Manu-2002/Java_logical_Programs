4b39be6c3fdf7ebca6f478c248218143c1a13c98
Manu-2002 <150989952+Manu-2002@users.noreply.github.com>
2024-11-06 12:27:04 +0530

    Add files via upload

---
+++ DAY1/Sample.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+import java.lang.String;
+class Sample
+	{
+		public static void main(String args[])
+			{
+				System.out.println("HELLO SRK");
+			}
+	}
+
+			
---
+++ DAY1/Sum.java
@@ -0,0 +1,11 @@
+class Sum
+{
+public static void main(String args[])
+{
+int emp=101;
+String name="KARUN";
+double sal=345.34;
+System.out.println("The given employee number is:"+emp);
+System.out.println("The given employee name is:"+name);
+System.out.println("The given employee salary is:"+sal);
+}}
