18f45123723aa5681ee00986220ea3df375cd959
Manu <150989952+Manu-2002@users.noreply.github.com>
2024-11-11 22:15:47 +0530

    Bubble.java

--- Day_4/Bubble.java
+++ Day_4/Bubble.java
@@ -5,28 +5,25 @@ class Bubble {
         Scanner mn = new Scanner(System.in);
         int n, i, j, t;
 
-        // Input array size
+
         System.out.println("Enter Array size:");
         n = mn.nextInt();
         int a[] = new int[n];
 
-        // Input array elements
         System.out.println("Enter the Array elements:");
         for (i = 0; i < n; i++) {
             a[i] = mn.nextInt();
         }
 
-        // Display elements before sorting
         System.out.println("The elements before sorting are:");
         for (i = 0; i < n; i++) {
             System.out.println(a[i]);
         }
 
-        // Bubble Sort algorithm
         for (i = 0; i < n - 1; i++) {
-            for (j = 0; j < n - 1 - i; j++) { // Inner loop to perform the comparisons
+            for (j = 0; j < n - 1 - i; j++) { 
                 if (a[j] > a[j + 1]) {
-                    // Swap the elements
+                  
                     t = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = t;
@@ -34,13 +31,10 @@ class Bubble {
             }
         }
 
-        // Display elements after sorting
+     
         System.out.println("The elements after sorting are:");
         for (i = 0; i < n; i++) {
             System.out.println(a[i]);
         }
-
-        // Close the scanner
-        mn.close();
     }
 }
